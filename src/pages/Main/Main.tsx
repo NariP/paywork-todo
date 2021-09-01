4c1303800305954a2afd36fe1b5cd09a245bacdb
박나리 <>
2021-09-02 00:08:48 +0900

    feat: 메인 컴포넌트 구현 #19

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,15 +1,68 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { Button } from 'components/Button';
+import { Background } from 'pages/Main';
+import { AiOutlineGoogle } from 'react-icons/ai';
+import { useUser } from 'utils/hooks';
 
 const Main = () => {
+  const { userLoading, login, logout, isAuthenticated } = useUser();
   return (
-    <div>
-      <div>main page</div>
-      <Link to={'/dashboard'}>
-        <button>dashboard</button>
-      </Link>
-    </div>
+    <Wrapper>
+      <WaveGroup>
+        <Background />
+      </WaveGroup>
+      <ContentsGroup>
+        <Text>타임테이블과 투두리스트를</Text>
+        <Text>한 번에 사용가능한</Text>
+        <Text>Istudy</Text>
+        {!isAuthenticated() && (
+          <LoginBtn
+            type="button"
+            name={!isAuthenticated() ? 'google' : 'logout'}
+            method={!isAuthenticated() ? login : logout}
+            disabled={userLoading}
+          >
+            <AiOutlineGoogle />
+            {userLoading ? '로딩 중...' : '로그인하여 시작하기'}
+          </LoginBtn>
+        )}
+      </ContentsGroup>
+    </Wrapper>
   );
 };
-
+const Wrapper = styled.div({
+  width: '100%',
+});
+const ContentsGroup = styled.div({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+});
+const Text = styled.h1({
+  color: '#fcfcfc',
+  textShadow: '#333 1px 0 10px',
+  fontSize: '3em',
+  marginBottom: '0.3em',
+});
+const WaveGroup = styled.div({
+  position: 'relative',
+});
+const LoginBtn = styled(Button)(({ theme }) => ({
+  display: 'flex',
+  background: theme.colors.bgColor,
+  color: theme.colors.textColor,
+  borderRadius: 10,
+  border: `1px solid ${theme.colors.normalAlpha}`,
+  boxShadow: `0 3px 5px ${theme.colors.normalAlpha}`,
+  fontSize: '1.5em',
+  ':active': {
+    boxShadow: `inset 0 3px 5px ${theme.colors.normalAlpha}`,
+  },
+  svg: {
+    color: theme.colors.primary,
+    marginRight: '0.2em',
+  },
+}));
 export default Main;
